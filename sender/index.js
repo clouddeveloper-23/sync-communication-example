d41b0b00fb6bb05c9f71c916bc39d426536af44a
Johan Kivi <johan.kivi@zocom.se>
2024-03-07 12:54:15 +0100

    update to API enpoints

--- sender/index.js
+++ sender/index.js
@@ -2,30 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-async function sendGetRequest() {
+app.get("/", async (req, res) => {
   const resp = await fetch(process.env.RECEIVER);
   const data = await resp.json();
-  console.log(data);
-}
+  res.send(data);
+});
 
-async function sendPostRequest() {
+app.post("/", async (req, res) => {
   const resp = await fetch(process.env.RECEIVER, {
     method: "post",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message: "A post message was sent." }),
   });
   const data = await resp.json();
-  console.log(data);
-}
-
-setInterval(async () => {
-  try {
-    await sendGetRequest();
-    await sendPostRequest();
-  } catch (error) {
-    console.error(error);
-  }
-}, 1000);
+  res.send(data);
+});
 
 app.listen(PORT, () => {
   console.log(`Sender service running on Port: ${PORT}.`);
